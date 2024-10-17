96e72db9e376cfac8d0a77ade9de4405f172d144
AfsanaSohana <sohanaafsana16@gmail.com>
2024-10-17 12:26:46 +0600

    16 0ct 24

--- database/migrations/2024_10_11_122510_create_exams_table.php
+++ database/migrations/2024_10_11_122510_create_exams_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('exams', function (Blueprint $table) {
             $table->id();
             $table->string('exam_name');
-            $table->time('duration');
+            $table->string('duration');
             $table->date('start_time');
             $table->date('end_time');
             $table->string('subject_id');
